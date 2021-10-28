0522548a252b349e1e20201b7772fa632985baf2
Stefan Obermeier <st.obermeier@seeburger.de>
2021-10-28 12:20:54 +0200

    Add K8s components

--- chaincode/tools/hlf-chaincode-definition-creator/README.md
+++ chaincode/tools/hlf-chaincode-definition-creator/README.md
@@ -1,10 +1,21 @@
-##
+# Chaincode description creator
 
+This tool creates a chaincode description and publishes it to a http data share.
+
+### Publish deployment description
 ```
-HOSTNAME=asset-transfer-basic.org1.example.com
+SHARED_FS=kubernetes.research.dev.seeburger.de:30080
+HOSTNAME=asset-transfer-basic.org6.example.com
 PORT=30489
 LABEL=basic_1.0
 
-./create-archive.sh asset-transfer-basic.org1.example.com 30489 basic_1.0 kubernetes.research.dev.seeburger.de:30080
+CC_DEPLOYER_POD=$(kubectl get pod -l app=cc-deployer -o jsonpath="{.items[0].metadata.name}")
+kubectl exec --stdin --tty $CC_DEPLOYER_POD -c cc-deployer -- /bin/sh /opt/create-archive.sh $HOSTNAME $PORT $LABEL $SHARED_FS
 ```
+
+### Deployment description location
+Location where the chaincode description (``chainecode_description.tgz`` and ``ccid.json``) can be downloaded from.
+``
+http://$SHARED_FS/cc_descriptions/${HOSTNAME}_$LABEL
+``
 
--- chaincode/tools/hlf-chaincode-definition-creator/create-archive.sh
+++ chaincode/tools/hlf-chaincode-definition-creator/create-archive.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
 mkdir -p target
 
 HOSTNAME=$1
@@ -9,10 +13,12 @@ SHARED_FS_HOST=$4
 create_description_file() {
   java -jar ./target/hlf-chaincode-definition-creator-1.0-jar-with-dependencies.jar $HOSTNAME $PORT $CC_LABEL
   cd target/${HOSTNAME}_$CC_LABEL/
-  tar  -czvf  codetar.tar.gz connection.json
-  tar -cvzf chainecode_description.tgz metadata.json codetar.tar.gz
-  DESCRIPTION_HASH=$(sha256sum codetar.tar.gz)
+  tar  -czvf  code.tar.gz connection.json
+  tar -cvzf chainecode_description.tgz metadata.json code.tar.gz
+  DESCRIPTION_HASH=$(sha256sum chainecode_description.tgz  | head -c 64)
   CC_ID=$CC_LABEL:$DESCRIPTION_HASH
+
+  echo "{ \"ccid\": \"$CC_ID\" }" > ccid.json
 }
 
 upload_ccdescription() {
@@ -23,6 +29,7 @@ upload_ccdescription() {
   curl --user $SHARED_FS_USER:$SHARED_FS_PW -X MKCOL http://$SHARED_FS_HOST/cc_descriptions
   curl --user $SHARED_FS_USER:$SHARED_FS_PW -X MKCOL http://$SHARED_FS_HOST/cc_descriptions/${HOSTNAME}_$CC_LABEL
   curl --user $SHARED_FS_USER:$SHARED_FS_PW -T chainecode_description.tgz http://$SHARED_FS_HOST/cc_descriptions/${HOSTNAME}_$CC_LABEL/
+  curl --user $SHARED_FS_USER:$SHARED_FS_PW -T description-hash.json      http://$SHARED_FS_HOST/cc_descriptions/${HOSTNAME}_$CC_LABEL/
 }
 
 create_description_file
