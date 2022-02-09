fa94702b53118178a37a7af96692fe4d880c7cb9
Stefan Obermeier <st.obermeier@seeburger.de>
2022-02-09 12:13:03 +0100

    Add install-chaincode command

---
+++ installer/commands/deploy-chaincode.sh
@@ -0,0 +1,47 @@
+SHARED_FS=hl-fabric-data-share-service:80
+
+function deploy() {
+
+	SHARED_FS=$1
+
+kubectl apply -f https://raw.githubusercontent.com/scray/scray-ledger/develop/chaincode/chaincode-external/k8s-service-eternal-chaincode.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/scray/scray-ledger/develop/chaincode/tools/hlf-chaincode-definition-creator/k8s-cc-deployer.yaml
+
+CC_HOSTNAME=asset-transfer-basic.org1.example.com
+CC_SERVICE_NAME=hl-fabric-cc-external-invoice
+CC_PORT=$(kubectl get service $CC_SERVICE_NAME -o jsonpath="{.spec.ports[?(@.name=='chaincode')].nodePort}")
+CC_LABEL=basic_1.0
+
+CC_DEPLOYER_POD=$(kubectl get pod -l app=cc-deployer -o jsonpath="{.items[0].metadata.name}")
+
+echo "Publish deployment description"
+kubectl exec --stdin --tty $CC_DEPLOYER_POD -c cc-deployer -- /bin/sh /opt/create-archive.sh $CC_HOSTNAME $CC_PORT $CC_LABEL $SHARED_FS
+
+
+}
+
+function startChaincode() {
+
+SHARED_FS=$1
+
+	# Get configuration
+CC_HOSTNAME=asset-transfer-basic.org1.example.com
+CC_LABEL=basic_1.0
+
+SHARED_FS_USER=scray
+SHARED_FS_PW=scray
+PKGID=$(curl -s  --user $SHARED_FS_USER:$SHARED_FS_PW http://$SHARED_FS/cc_descriptions/${CC_HOSTNAME}_$CC_LABEL/description-hash.json 2>&1 | jq -r '."description-hash"')
+PKGID=basic_1.0:ee12ea689d8dfd99d62b4ffe7f33ab8a10cd5a949fbf5c26199b851644f941ed
+
+	kubectl delete configmap invoice-chaincode-external
+	kubectl create configmap invoice-chaincode-external \
+		 --from-literal=chaincode_id=$PKGID
+
+	kubectl apply -f ../../chaincode/chaincode-external/k8s-external-chaincode.yaml
+
+}
+
+deploy $SHARED_FS_HOST
+startChaincode $SHARED_FS
+
---
+++ installer/commands/install-chaincode.sh
@@ -0,0 +1,66 @@
+SHARED_FS=hl-fabric-data-share-service:80
+
+function installChaincode() {
+  PEER_NAME=$1
+  CHANNEL_NAME=$2
+  SHARED_FS=$3
+
+CC_HOSTNAME=asset-transfer-basic.org1.example.com
+
+ORDERER_NAME=orderer.example.com
+IP_CC_SERVICE=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[?(@.type=='InternalIP')].address}")         # Host where the chaincode is running
+PEER_POD=$(kubectl get pod -l app=$PEER_NAME -o jsonpath="{.items[0].metadata.name}")
+ORDERER_IP=$(kubectl get pods  -l app=orderer-org1-scray-org -o jsonpath='{.items[*].status.podIP}')
+ORDERER_LISTEN_PORT=$(kubectl get service orderer-org1-scray-org -o jsonpath="{.spec.ports[?(@.name=='orderer-listen')].nodePort}")
+ORDERER_HOST=orderer.example.com
+EXT_PEER_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[?(@.type=='InternalIP')].address}") 
+
+ORDERER_PORT=$(kubectl get service orderer-org1-scray-org -o jsonpath="{.spec.ports[?(@.name=='orderer-listen')].nodePort}")
+ORDERER_PORT=30081
+ORDERER_IP=$(kubectl get pods  -l app=orderer-org1-scray-org -o jsonpath='{.items[*].status.podIP}')
+
+CC_LABEL=basic_1.0
+PKGID=basic_1.0:ee12ea689d8dfd99d62b4ffe7f33ab8a10cd5a949fbf5c26199b851644f941ed
+
+kubectl exec --stdin --tty $PEER_POD -c scray-peer-cli -- /bin/sh \
+    /mnt/conf/install_and_approve_cc.sh \
+        $IP_CC_SERVICE \
+        $ORDERER_IP \
+        $ORDERER_HOST \
+        $ORDERER_PORT \
+        $CHANNEL_NAME \
+        $PKGID \
+        $CC_HOSTNAME \
+        $CC_LABEL \
+        $SHARED_FS
+
+kubectl exec --stdin --tty $PEER_POD -c scray-peer-cli -- /bin/sh /mnt/conf/peer/cc_commit.sh  $CHANNEL_NAME $PKGID
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --peer-name )   shift
+ 	   PEER_NAME=$1
+	;;
+        --channel-name )   shift
+           CHANNEL_NAME=$1
+	;;
+        --share )   shift
+	   SHARED_FS=$1
+        ;;
+
+    esac
+    shift
+done
+
+if [ -z "$PEER_NAME" ]
+then
+ echo "peer name is missing" ${usage}
+ exit 1
+elif [ -z "$CHANNEL_NAME" ]
+then
+  echo "channel name is missing" ${usage}
+  exit 1
+else
+ installChaincode $PEER_NAME $CHANNEL_NAME $SHARED_FS 
+fi
