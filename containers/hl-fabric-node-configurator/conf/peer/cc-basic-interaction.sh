a961dd256bb4088433c6d2a98156258ae998f7ef
Stefan Obermeier <st.obermeier@seeburger.de>
2022-02-12 02:31:43 +0100

    Clean up before creating chaincode container

--- containers/hl-fabric-node-configurator/conf/peer/cc-basic-interaction.sh
+++ containers/hl-fabric-node-configurator/conf/peer/cc-basic-interaction.sh
@@ -8,6 +8,7 @@ export CORE_PEER_MSPCONFIGPATH=/mnt/conf/organizations/peerOrganizations/$HOSTNA
 echo "Call init method of chain code basic"
 peer chaincode invoke -o orderer.example.com:30081 --waitForEventTimeout 60s --tls --cafile /tmp/tlsca.example.com-cert.pem -C $CHANNEL_ID -n basic -c '{"function":"InitLedger","Args":[]}'
 
+sleep 5s
 echo "Read all assets"
 peer chaincode query -C $CHANNEL_ID -n basic -c '{"Args":["GetAllAssets"]}'
 
--- installer/commands/deploy-chaincode.sh
+++ installer/commands/deploy-chaincode.sh
@@ -28,6 +28,9 @@ startChaincode() {
 
 SHARED_FS=$1
 
+# Clean up
+kubectl delete deployment invoice-chaincode-external
+
 	# Get configuration
 CC_HOSTNAME=asset-transfer-basic.org1.example.com
 CC_LABEL=basic_1.0
