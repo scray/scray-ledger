18f18d5477823134c54a89d6946e8e95a68f4d95
JohannesStrassner <j.strassner@seeburger.de>
2022-04-13 16:31:04 +0000

    updated notebooks

---
+++ chaincode/otest/docker_deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source .env
+
+REPO_URL=scrayorg
+VERISON=1.2
+
+pushDockerHub() {
+    echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    echo "$DOCKER_TOKEN"
+    echo "$DOCKER_USERNAME"
+    docker build -t  "$REPO_URL/$DOCKER_IMAGE_NAME:$VERISON" .
+    docker push  "$REPO_URL/$DOCKER_IMAGE_NAME:$VERISON"
+}
+
+pushLocal() {
+    docker build -t "$REPO_URL/$DOCKER_IMAGE_NAME:$VERISON" .
+    docker push "$REPO_URL/$DOCKER_IMAGE_NAME:$VERISON"
+}
+
+# Remove runtime data
+cleanUp() {
+    echo
+}
+
+usage() {
+    echo "usage: push container to docker registry [[-h push to docker hub ] [-l push to local registry]]"
+}
+
+if [[ -z "$1" ]]
+then
+    usage
+fi
+                                echo Build image  "$DOCKER_IMAGE_NAME"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -l | --local )   shift
+                                REPO_URL=$1
+                                echo Build image  "$DOCKER_IMAGE_NAME"
+                                cleanUp
+                                pushLocal
+                                ;;
+        -h | -docker-hub )      shift
+                                echo Build image  "$DOCKER_IMAGE_NAME"
+                                cleanUp
+                                pushDockerHub
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
---
+++ chaincode/otest/go.mod
@@ -0,0 +1,11 @@
+module github.com/hyperledger/fabric-samples/token-erc-721/chaincode-go
+
+go 1.17
+
+require (
+	github.com/hyperledger/fabric-chaincode-go v0.0.0-20220131132609-1476cf1d3206
+	github.com/hyperledger/fabric-contract-api-go v1.1.1
+	github.com/hyperledger/fabric-protos-go v0.0.0-20220202165055-956c75de7b17
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/tools v0.1.10 // indirect
+)
