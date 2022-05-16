38e7ed40e5dc8cd99746793beab1624836ca3e0d
Stefan Obermeier <st.obermeier@seeburger.de>
2022-05-16 16:19:29 +0200

    Improve log messages and wait for orderer

--- installer/commands/add-peer.sh
+++ installer/commands/add-peer.sh
@@ -13,13 +13,19 @@ function addPeer() {
   ORDERER_HOSTNAME=orderer.example.com
   ORDERER_PORT=30081
   ORDERER_POD=$(kubectl get pod -l app=orderer-org1-scray-org -o jsonpath="{.items[0].metadata.name}")
-  echo FFF
+
+  echo "Inform existing nodes" 
   kubectl exec --stdin --tty $ORDERER_POD -c scray-orderer-cli  -- /bin/sh /mnt/conf/orderer/scripts/inform_existing_nodes.sh $ORDERER_IP $CHANNEL_NAME $PEER_NAME $SHARED_FS_HOST $EXT_PEER_IP $PEER_HOST_NAME
+
+
+  sleep 10s
 
   PEER_POD_NAME=$(kubectl get pod -l app=$PEER_NAME -o jsonpath="{.items[0].metadata.name}")
   ORDERER_PORT=$(kubectl get service orderer-org1-scray-org -o jsonpath="{.spec.ports[?(@.name=='orderer-listen')].nodePort}")
   ORDERER_PORT=30081
   PEER_PORT=$(kubectl get service $PEER_NAME -o jsonpath="{.spec.ports[?(@.name=='peer-listen')].nodePort}")
+
+  echo "Join $CHANNEL_NAME"
   kubectl exec --stdin --tty $PEER_POD_NAME  -c scray-peer-cli -- /bin/sh /mnt/conf/peer_join.sh $ORDERER_IP  $ORDERER_HOSTNAME $ORDERER_PORT $CHANNEL_NAME $SHARED_FS_HOST $EXT_PEER_IP
 }
 
