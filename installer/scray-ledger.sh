fb4184bc791f9eb55acfe00505301b447bc4baf3
Stefan Obermeier <st.obermeier@seeburger.de>
2023-07-07 10:55:12 +0200

    Add test sans

--- installer/scray-ledger.sh
+++ installer/scray-ledger.sh
@@ -36,6 +36,7 @@ createOrderer() {
   kubectl create configmap hl-fabric-orderer \
   	 --from-literal=hostname=$HOST_NAME \
 	 --from-literal=org_name=$ORDERER_NAME \
+	 --from-literal=sans=peer1.internal.hsa.blockchain.s-node.de,peer1.hsa.blockchain.s-node.de,orderer1.hsa.blockchain.s-node.de,orderer1.internal.hsa.blockchain.s-node.de \
 	 --from-literal=ORDERER_GENERAL_LOCALMSPID=${ORDERER_NAME}MSP \
 	 --from-literal=NODE_TYPE=orderer
 
